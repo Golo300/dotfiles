6c1b103d4fa9947e5ce112237d9b0336eb2c1f34
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-07-23 13:31:51 +0200

    feat(env): add enviroment variabels

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -32,8 +32,6 @@
       };
       wifi-security = {
         key-mgmt = "wpa-eap";
-      };
-      "802-1x" = {
         eap = "ttls;";
         identity = "gpn";
         password = "gpn";
@@ -52,6 +50,19 @@
         method = "auto";
       };
     };
+  };
+
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME = "$HOME/.local/bin";
+    PATH = [
+      "${XDG_BIN_HOME}"
+    ];
   };
 
   hardware.pulseaudio.enable = true;
