187186b0edc3519d23744fd13c7949a5fbca4bc4
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-08-14 00:59:35 +0200

    feat(select project): select project and flake update

--- home/timl/programs/zsh.nix
+++ home/timl/programs/zsh.nix
@@ -1,37 +1,43 @@
 { pkgs, ... }:
 
 {
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      # shortcuts
-      "ll" = "ls -la";
-      ".." = "cd ..";
-      "conf" = "cd ~/dotfiles\n nvim";
-      # other stuff
-      hibernate = "systemctl hibernate";
-      repo = "cd $(reporer)";
+  programs.zsh =
+    let
+      select-project = import ../../../pkgs/select-project.nix { inherit pkgs; };
 
-      wgu = "systemctl start wg-quick-wg0";
-      wgd = "systemctl stop wg-quick-wg0";
-      wgs = "systemctl status wg-quick-wg0";
-    };
-    enableCompletion = true;
-
-    zplug = {
+    in
+    {
       enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      ];
+      shellAliases = {
+        # shortcuts
+        "ll" = "ls -la";
+        ".." = "cd ..";
+        "conf" = "cd ~/dotfiles\n nvim";
+        # other stuff
+        hibernate = "systemctl hibernate";
+        repo = "cd $(reporer)";
+        sp = ''${select-project}/bin/select-project'';
 
-    };
+        wgu = "systemctl start wg-quick-wg0";
+        wgd = "systemctl stop wg-quick-wg0";
+        wgs = "systemctl status wg-quick-wg0";
+      };
+      enableCompletion = true;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "battery"
-      ];
-      theme = "bira";
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        ];
+
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "battery"
+        ];
+        theme = "bira";
+      };
     };
-  };
 }
