5a87e2fb6faf61144995c746f225a8c3ccd98d19
Tim Lanzinger <tim.lanzinger@gmx.de>
2025-01-09 15:42:29 +0100

    fix(format): fix format

--- hosts/common/core/boot.nix
+++ hosts/common/core/boot.nix
@@ -1,11 +1,11 @@
 { config, inputs, pkgs, agenix, lib, ... }:
 {
-      boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
-      boot.initrd.kernelModules = [ ];
-      boot.kernelModules = [ "kvm-amd" ];
-      boot.extraModulePackages = [ ];
-      boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
