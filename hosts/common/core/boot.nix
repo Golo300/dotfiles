4c810cd16dfd3effb169698529ead5bbe2a0c55e
Ruben Hoenle <git@hoenle.xyz>
2025-02-25 22:22:26 +0100

    chore(fmt): apply code format

--- hosts/common/core/boot.nix
+++ hosts/common/core/boot.nix
@@ -2,10 +2,10 @@
 {
   boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [
-        "vfio_pci"
-        "vfio"
-        "vfio_iommu_type1"
-      ];
+    "vfio_pci"
+    "vfio"
+    "vfio_iommu_type1"
+  ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
--- modules/printing.nix
+++ modules/printing.nix
@@ -14,7 +14,7 @@
         model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
         deviceUri = "socket://10.11.42.10";
       }
-        ];
+    ];
   };
 }
 
