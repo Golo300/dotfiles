580ac5f0d22dd30f290ef2351a551ccd7b493f54
Ruben Hoenle <git@hoenle.xyz>
2025-02-25 22:22:02 +0100

    feat(printing): add space 2d printer

---
+++ modules/printing.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      hplip
+      #brlaser
+    ];
+  };
+  hardware.printers = {
+    ensureDefaultPrinter = "Space-2D-Printer";
+    ensurePrinters = [
+      {
+        name = "Space-2D-Printer";
+        model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
+        deviceUri = "socket://10.11.42.10";
+      }
+        ];
+  };
+}
+
